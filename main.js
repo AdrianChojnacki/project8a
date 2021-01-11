eb8051bef138141dedb708f75b4e2c4e873b7c05
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-01-11 07:53:21 +0100

    stage 2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const spnText = document.querySelector(`.text`);
+const spnCursor = document.querySelector(`.cursor`);
 const txt =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia veniam quae? Earum minus provident quae dicta, doloremque officia dolore, quasi molestiae, saepe ipsa nesciunt! Saepe rerum consequatur quam soluta.";
 
@@ -6,11 +7,20 @@ const txt =
 let indexText = 0;
 const time = 40;
 
-// Function
+// Letter function
 const addLetter = () => {
   spnText.textContent += txt[indexText];
   indexText++;
   if (indexText === txt.length) clearInterval(indexTyping);
 };
 
-const indexTyping = setInterval(addLetter, 40);
+const indexTyping = setInterval(addLetter, time);
+
+// Cursor funcion
+const cursorAnimation = () => {
+  spnCursor.classList.toggle("active");
+};
+
+setTimeout(function () {
+  setInterval(cursorAnimation, 400);
+}, txt.length * time);
